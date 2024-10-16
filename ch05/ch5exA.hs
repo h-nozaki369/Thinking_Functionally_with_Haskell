0dffc801c9d3e4603b397fe405e40743485ec3fc
Hajime Nozaki <nozaki369@outlook.jp>
2024-10-16 23:43:23 +0900

    Add ch5exA

--- ch05/ch5exA.hs
+++ ch05/ch5exA.hs
@@ -5,4 +5,12 @@ add1 :: Matrix Int -> Matrix Int
 add1 = map (map (+1))
 
 addM :: Matrix Int -> Matrix Int -> Matrix Int
-addM xm ym = zipWith (+) <$> xm <*> ym
+addM = zipWith (zipWith (+))
+
+transpose :: [[a]] -> [[a]]
+transpose [xs] = [[x] | x <- xs]
+transpose (xs : xss) = zipWith (:) xs (transpose xss)
+
+mulM :: Matrix Int -> Matrix Int -> Matrix Int
+mulM xss yss = [[sum (zipWith (*) xs ys')] | xs <- xss, ys' <- yss']
+               where yss' = transpose yss
